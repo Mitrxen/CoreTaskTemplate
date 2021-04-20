1229a62a3cb3a1fdc2cb64d413a4e71d8026056f
Rick <Rick@Rick-PC>
2021-04-20 18:20:09 +0500

    Practice 1.1.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "123456";
+    
+    
+    
+    
+    public static Connection getConnect() {
+    	return getConnect(URL, NAME, PASSWORD);
+    }
+    
+    public static Connection getConnect(String url, String user, String pass) {
+    	Connection conn = null;
+    	
+    	try {
+			conn = DriverManager.getConnection(url, user, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+    	return conn;
+    }
+    
+
 }
